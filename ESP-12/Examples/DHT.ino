be76c38fcf85c3f944b0933a3302fa2b627446f7
SmarDen <85669477+SmarDen-tech@users.noreply.github.com>
2022-01-19 14:40:06 +0530

    Update DHT.ino

--- ESP-12/Examples/DHT.ino
+++ ESP-12/Examples/DHT.ino
@@ -1,128 +1,50 @@
-//=================All Library Files===============================
-#include <ESP8266WiFi.h>
-#include <PubSubClient.h>
-#include<TaskScheduler.h>
-#include "DHT.h"
-
-
+#include "smarden_tech.h"
+//==================Enter the details according to your instance=================================================
+String deviceid = "----------------------";                    // Enter the device ID
+String accesskey = "---------------------------";               // Enter the Access Key
+char* wifi_ssid = "---------------";                            // Enter the WiFi SSID
+char* wifi_pass = "----------";                                 // Enter WiFi Password
 #define DHTPIN D2
 #define DHTTYPE DHT11
-#define refresh_time 5000 //define the sensor value refresh time in miliseconds
-//=========================Enter Device ID==========================
-String DeviceID = "........"; // Enter you device ID 
-
-//=======================WiFi and Mqtt Credentials=================
-const char* ssid = "............."; // Wifi SSID
-const char* password = ".........";   // WiFi Pssword
-const char* mqtt_server = "login.smarden.tech";  // Mqtt Server Address, please do not change
-const char* username = "smardentech";    //your instance username
-const char* mqtt_pass = "smardentech1@3$";      // your instance password
-
-//=======================Pub/sub Topics==============================
-String Pub_topic = "smardentechresponse";  // Publishing topic, click on the element properties to find your publishing topic. 
-
-//=====================Add element ID=================================
-String TempID =  "........................."; // Element ID for the Temperature
-String HumID = "..........................."; // Element ID for the Humidity 
-
-//=====================Connection Status messages=====================
-String willmessage = "dev_status " + DeviceID + " offline"; //do not change
-String mqttstatus = "dev_status " + DeviceID + " online";  // do not change
-
-const int RSSI_MAX = -50; // define maximum strength of signal in dBm
-const int RSSI_MIN = -100; // define minimum strength of signal in dBm
-String signalstrength ;
-
-//===================TaskSchedular==========================================
-void temperature();
-Task T1(10000, TASK_FOREVER, &temperature);
-
-Scheduler runner;
-WiFiClient espClient;
-PubSubClient client(espClient);
+#define refresh_time 10000 
+void callback(char* topic, byte* Payload, unsigned int length);
 DHT dht(DHTPIN, DHTTYPE);
-
-void setup_wifi() {
-
-  delay(10);
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  randomSeed(micros());
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-  Serial.print(WiFi.RSSI());//Signal strength in dBm
-  Serial.print("dBm (");
-  Serial.print(dBmtoPercentage(WiFi.RSSI()));//Signal strength in %
-  Serial.print("% )");
-  signalstrength = "sig_strength " + DeviceID + " " + String(dBmtoPercentage(WiFi.RSSI())) + "%";
-}
-
-
-//===============================================MQTT Reconnect function==============================================
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Create a random client ID
-    String clientId = "ESP8266Client-";
-    clientId += String(random(0xffff), HEX);
-    // Attempt to connect
-    if (client.connect(clientId.c_str(), username, mqtt_pass, Pub_topic.c_str(), 1, 0, willmessage.c_str())) {
-      Serial.println("connected");
-      client.publish(Pub_topic.c_str(), mqttstatus.c_str(), false);
-      client.publish(Pub_topic.c_str(), signalstrength.c_str(), false);
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
-}
-
-//=========================Convert dBm to Percentage============================================
-int dBmtoPercentage(int dBm)
-{
-  int quality;
-  if (dBm <= RSSI_MIN) {
-    quality = 0;
-  }
-  else if (dBm >= RSSI_MAX) {
-    quality = 100;
-  }
-  else {
-    quality = 2 * (dBm + 100);
-  }
-  return quality;
-}//dBmtoPercentage
-
-//========================================Setup Part============================================
+//========================Enter Element IDs=======================================================================
+ String tempid = "--------------";
+ String humid = "---------------";
+//=============================Setup Part==========================================================================
 void setup() {
+  dht.begin();          
   Serial.begin(115200);
-  setup_wifi();
-  runner.init(); // Initializing the runner and enebling the task 
-  Serial.println("Initialized scheduler");
-  runner.addTask(T1);
-  Serial.println("Added T1");
-  T1.enable();
-  dht.begin();
-  client.setServer(mqtt_server, 1884); //mqtt port will be 1884
+  wifi_connect(wifi_ssid, wifi_pass); //call this function to connect to your WiFi
+  mqtt_connect(accesskey,deviceid);   //call this function to connect to Smarden.tech MQTT server   
+  task();                             //start the task funtion to get the live device status 
+  client.setCallback(callback);       //This "callback" function will be called everytime you receive data from the smarden.tech server
 }
-
+//=============================Loop Part============================================================================
+void loop() {
+  runner.execute();
+  if (!client.connected()) {              // if the client is disconnected, try to connect again to the smarden.tech cloud
+    mqtt_connect(accesskey,deviceid); 
+  }
+  client.loop();
+  temperature();
+}
+//================================MQTT Callback Function=============================================================
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived");
+  char sub[1000];
+  int c = 0;
+  char* payl = (char *)payload;
+  while (c < length) {
+    sub[c] = payl[c];
+    c++;}
+  sub[c] = '\0';
+  Serial.println(sub); 
+  String msg = sub;                               //receiving the payloads from the server
+  control(D4,msg,1,"on");                        //Call this function to control the GPIO pins on the device 
+  publish_sw_data(deviceid, ledid, msg);         //Publishing the switch state to the server
+}
 //===============================Temperature Function ========================================
 void temperature () {
   delay(refresh_time);
@@ -134,21 +56,6 @@ void temperature () {
   Serial.print(F("%  Temperature: "));
   Serial.print(t);
   Serial.print(F("°C "));
-  
-  Serial.println(" ");
-  Serial.print("Publishing Device status and Signal Strength");
-  client.publish(Pub_topic.c_str(), signalstrength.c_str(), false); // Publish the Signal Strength 
-  
-  String Temp_msg = "smarden_dev_sens " + TempID + " " + String(t);
-  String Hum_msg = "smarden_dev_sens " + HumID + " " + String(h);
-  client.publish(Pub_topic.c_str(), Temp_msg.c_str(), true); // Publish the temperature to the server
-  client.publish(Pub_topic.c_str(), Hum_msg.c_str(), true); // publish the Humidity to the server
-}
-//===================================Loop Part======================================================
-void loop() {
-  runner.execute();
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
+  publish_sn_data(deviceid,tempid,t); // Publish the temperature to the server
+  publish_sn_data(deviceid,humid,h); // publish the Humidity to the server
 }
